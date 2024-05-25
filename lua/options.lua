06be40e742228600045e7ae46a7f74cdf5c2bcf9
Linhan Wang <wanglinhan.pku@gmail.com>
2024-05-24 22:32:15 -0400

    enable folding with treesitter

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,12 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "120"
+
+-- enable folding using treesitter
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
 vim.lsp.set_log_level "error"
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
