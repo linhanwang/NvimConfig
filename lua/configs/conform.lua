ff73774224998c8d48ccb7212e829ddb0ffbc5b3
Linhan Wang <wanglinhan.pku@gmail.com>
2024-09-04 17:23:19 -0400

    add cpp lsp support

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -2,6 +2,7 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     python = { "isort", "ruff_format" },
+    cpp = { "clang-format" },
     css = { "prettier" },
     html = { "prettier" },
     javascript = { "prettier" },
