304357a7c86d36c09e5d219c454933f900da1dca
Linhan Wang <wanglinhan.pku@gmail.com>
2025-01-12 16:22:21 +0000

    change to light them

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,22 +1,21 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
-	theme = "gruvbox",
-  statusline = {
-    theme = "default",
-  },
-  nvdash = {
-    load_on_startup = true,
-  },
+M.base46 = {
+  theme = "ayu_light",
+}
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+M.ui = {
+  statusline = {
+    theme = "vscode_colored",
+  },
+}
+
+M.nvdash = {
+  load_on_startup = true,
 }
 
 return M
