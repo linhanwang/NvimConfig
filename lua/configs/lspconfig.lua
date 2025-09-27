0460b549dcddeb3485b0e195f2dd1550a8865822
Linhan Wang <wanglinhan.pku@gmail.com>
2025-09-27 19:05:50 -0400

    lsp require(configs.lspconfig) deprecation

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,21 +3,22 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "ts_ls", "pyright" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config(lsp, {
     on_attach = on_attach,
     on_init = on_init,
     capabilities = capabilities,
-  }
+  })
+  vim.lsp.enable(lsp)
 end
 
-lspconfig["clangd"].setup {
+vim.lsp.config("clangd", {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
   cmd = { "clangd", "--header-insertion=never" },
-}
+})
+vim.lsp.enable("clangd")
